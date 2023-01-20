ae14f77e337b7fa71808339702ef828311e8e9a4
hiluan <apt.muenchen@gmail.com>
2023-01-20 00:24:27 -0800

    Customers Page

--- src/scenes/products/index.jsx
+++ src/scenes/products/index.jsx
@@ -87,9 +87,6 @@ const Product = ({
 const Products = () => {
   const { data, isLoading } = useGetProductsQuery();
   const isNonMobile = useMediaQuery("(min-width: 1000px)");
-
-  console.log("🚀 ~ data", data);
-
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="PRODUCTS" subtitle="See your list of products." />
