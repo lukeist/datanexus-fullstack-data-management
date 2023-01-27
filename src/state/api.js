348ccba40d75589c8a0f133a62c0e94239df7275
hiluan <apt.muenchen@gmail.com>
2023-01-27 00:32:58 -0800

    Overview Page

--- src/state/api.js
+++ src/state/api.js
@@ -5,7 +5,14 @@ export const api = createApi({
     baseUrl: process.env.REACT_APP_BASE_URL,
   }),
   reducerPath: "adminApi",
-  tagTypes: ["User", "Products", "Customers", "Transactions", "Geography"],
+  tagTypes: [
+    "User",
+    "Products",
+    "Customers",
+    "Transactions",
+    "Geography",
+    "Sales",
+  ],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `/general/user/${id}`,
@@ -36,6 +43,10 @@ export const api = createApi({
       query: () => "client/geography",
       providesTags: ["Geography"],
     }),
+    getSales: build.query({
+      query: () => "sales/sales",
+      providesTags: ["Sales"],
+    }),
   }),
 });
 
@@ -45,4 +56,5 @@ export const {
   useGetCustomersQuery,
   useGetTransactionsQuery,
   useGetGeoGraphyQuery,
+  useGetSalesQuery,
 } = api;
