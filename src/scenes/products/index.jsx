f6b8cfefebdb910ab13ac9b1a68b6ee79d4975f5
hiluan <apt.muenchen@gmail.com>
2023-02-17 17:13:29 -0800

    Fix useMediaQuery Bug

--- src/scenes/products/index.jsx
+++ src/scenes/products/index.jsx
@@ -87,8 +87,8 @@ const Product = ({
 
 const Products = () => {
   const { data, isLoading } = useGetProductsQuery();
-  if (!data || isLoading) return <Loading />;
   const isNonMobile = useMediaQuery("(min-width: 1000px)");
+  if (!data || isLoading) return <Loading />;
 
   return (
     <Box m="1.5rem 2.5rem">
